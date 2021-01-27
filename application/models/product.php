a34d4c8bc10cf68801daab93803ff16ed6fb530f
Adonys <avaldesa007@gmail.com>
2021-01-27 03:41:16 -0400

    updates

--- application/models/product.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Product extends CI_Model{
-   
-    //insert transaction data
-    public function insertTransaction($data = array()){
-        $insert = $this->db->insert('payments',$data);
-        return $insert?true:false;
-    }
-}
--- application/views/paypal/cancel.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <h3>Dear Member</h3>
-    <p>We are sorry! Your last transaction was cancelled.</p>
-</div>
